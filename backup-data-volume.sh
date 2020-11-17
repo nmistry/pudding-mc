c56833ce71e546aa466126ee4d491db73c8383b8
Nicholas Mistry <nick@mistry.com>
2020-11-17 17:43:52 -0600

    JMX, Restart script, and updated backup script.

--- backup-data-volume.sh
+++ backup-data-volume.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 TIMESTAMP=$( date -Iseconds ) 
-
+docker-compose down
 docker run --rm \
 	--volume mc_mc-paper:/data \
 	--volume /nas/Media/backup/mc-pudding:/backup \
 	ubuntu \
 	tar jcvf /backup/${TIMESTAMP}-mc_mc-paper.tbz /data 
+docker-compose up -d
